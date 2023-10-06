226eb0645a4f87b60365d75824a71a88f5efa7fd
Nathanael Putra Agusalim <nagusalim@student.ciputra.ac.id>
2023-10-06 23:51:40 +0700

    Initial Commit

---
+++ CalculatorApp/CalculatorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorAppApp.swift
+//  CalculatorApp
+//
+//  Created by MacBook Pro on 06/10/23.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
