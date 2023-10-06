af922225dbd6f70d350fab0e3247bd43e2ce278f
Nathanael Putra Agusalim <nagusalim@student.ciputra.ac.id>
2023-10-07 02:47:10 +0700

    First Commit

--- CalculatorApp/ContentView.swift
+++ CalculatorApp/ContentView.swift
@@ -8,14 +8,139 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var currentInput = "0.0"
+    @State private var previousNumber = 0.0
+    @State private var currentNumber = 0.0
+    @State private var selectedOperator = ""
+    @State private var isTyping = false
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(spacing: 70) {
+            Text(currentInput)
+                .font(.largeTitle)
+                .padding()
+            
+            VStack(spacing: 100) {
+                HStack(spacing: 70) {
+                    Button("AC") { clear() }
+                    Button("+/-") { toggleSign() }
+                    Button("%") { calculatePercentage() }
+                    Button("/") { operatorTapped("/") }
+                        .foregroundColor(.red)
+                }
+                
+                HStack(spacing: 70) {
+                    Button("7") { digitTapped("7") }
+                        .foregroundColor(.black)
+                    Button("8") { digitTapped("8") }
+                        .foregroundColor(.black)
+                    Button("9") { digitTapped("9") }
+                        .foregroundColor(.black)
+                    Button("*") { operatorTapped("*") }
+                        .foregroundColor(.red)
+                }
+                
+                HStack(spacing: 70) {
+                    Button("4") { digitTapped("4") }
+                        .foregroundColor(.black)
+                    Button("5") { digitTapped("5") }
+                        .foregroundColor(.black)
+                    Button("6") { digitTapped("6") }
+                        .foregroundColor(.black)
+                    Button("-") { operatorTapped("-") }
+                        .foregroundColor(.red)
+                }
+                
+                HStack(spacing: 70) {
+                    Button("1") { digitTapped("1") }
+                        .foregroundColor(.black)
+                    Button("2") { digitTapped("2") }
+                        .foregroundColor(.black)
+                    Button("3") { digitTapped("3") }
+                        .foregroundColor(.black)
+                    Button("+") { operatorTapped("+") }
+                        .foregroundColor(.red)
+                }
+                
+                HStack(spacing: 70) {
+                    Button("0") { digitTapped("0") }
+                        .frame(width: 90)
+                        .foregroundColor(.black)
+                    Button(".") { digitTapped(".") }
+                        .foregroundColor(.black)
+                        
+                        
+                    Button("=") { calculate() }
+                        .foregroundColor(.red)
+                }
+            }
         }
         .padding()
+    }
+
+    func digitTapped(_ digit: String) {
+        if isTyping {
+            currentInput += digit
+        } else {
+            currentInput = digit
+            isTyping = true
+        }
+    }
+
+    func operatorTapped(_ op: String) {
+        if selectedOperator.isEmpty {
+            previousNumber = Double(currentInput) ?? 0
+            currentInput = "\(previousNumber)"
+        } else {
+            calculate()
+        }
+        selectedOperator = op
+        isTyping = false
+    }
+
+    func calculate() {
+        if let current = Double(currentInput) {
+            switch selectedOperator {
+            case "+":
+                previousNumber += current
+            case "-":
+                previousNumber -= current
+            case "*":
+                previousNumber *= current
+            case "/":
+                if current != 0 {
+                    previousNumber /= current
+                } else {
+                    currentInput = "Error"
+                    return
+                }
+            default:
+                break
+            }
+            currentInput = "\(previousNumber)"
+            selectedOperator = ""
+        }
+        isTyping = false
+    }
+    
+    func toggleSign() {
+        if let value = Double(currentInput) {
+            currentInput = "\(value * -1)"
+        }
+    }
+    
+    func calculatePercentage() {
+        if let value = Double(currentInput) {
+            currentInput = "\(value / 100)"
+        }
+    }
+
+    func clear() {
+        currentInput = "0"
+        previousNumber = 0.0
+        currentNumber = 0.0
+        selectedOperator = ""
+        isTyping = false
     }
 }
 
